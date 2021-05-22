e2bf348b021937e538eabcd22cc1683609dff39a
krishpranav <krisna.pranav@gmail.com>
2021-05-22 12:11:04 +0530

    [+] compress script [+]

--- lib/rex/powerhshell/command.rb
+++ lib/rex/powerhshell/command.rb
@@ -14,4 +14,13 @@ module Command
     def self.decode_script(script_in)
         Rex::Powershell::Script.new(script_in).decode_code
     end
+
+    def self.compress_script(script_in, eof=nil, opts={})
+        psh = Rex::Powershell::Script.new(script_in)
+        psh.strip_comments if opts[:strip_comments]
+        psh.strip_whitespaces if opts[:strip_whitespaces]
+        psh.sub_vars if opts[:sub_vars]
+        psh.sub_funcs if opts[:sub_funcs]
+        psh.compress_code(eof)
+    end
     
