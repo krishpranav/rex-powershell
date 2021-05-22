e0b43c782a4138d79dae2940d42ee3a9b0e067d6
krishpranav <krisna.pranav@gmail.com>
2021-05-22 12:49:53 +0530

    [+] requires [+]

---
+++ lib/rex/powershell.rb
@@ -0,0 +1,13 @@
+# requires
+require 'rex/powershell/version'
+require 'rex/powershell/output'
+require 'rex/powershell/parser'
+require 'rex/powershell/obfu'
+require 'rex/powershell/function'
+require 'rex/powershell/param'
+require 'rex/powershell/script'
+require 'rex/powershell/templates'
+require 'rex/powershell/payload'
+require 'rex/powershell/psh_methods'
+require 'rex/powershell/command'
+
